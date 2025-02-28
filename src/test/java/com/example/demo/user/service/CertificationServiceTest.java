07399b10674248b126a99b2e9931f94cab819844
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-02-28 16:01:55 +0900

    :sparkles: feature: mailSender 의존성 분리

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지() {
+        //given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        //when
+        certificationService.send("f@naver.com", 2, "aaaa-aaaa");
+
+        //then
+        Assertions.assertThat(fakeMailSender.email).isEqualTo("f@naver.com");
+        Assertions.assertThat(fakeMailSender.title).isEqualTo("Please your email address");
+        Assertions.assertThat(fakeMailSender.content).isEqualTo("Please Clock the following link to certify your email address: http://localhost:8080/api/users/2/verify?certificationCode=aaaa-aaaa");
+
+    }
+
+}
