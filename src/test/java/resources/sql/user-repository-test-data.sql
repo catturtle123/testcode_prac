8afa225fb5fbc7cda63460d5c4b744e07c9d0327
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-02-28 11:41:23 +0900

    :sparkles: feature: userRepository test

---
+++ src/test/java/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (2, 'a@naver.com', 'jamey', 'Seoul', 'aaaa-aaaa', 'ACTIVE', 0);
+INSERT INTO `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+VALUES (3, 'b@naver.com', 'jamey1', 'Seoul1', 'aaaa-aaaaa', 'PENDING', 0);
