0716c42fc73602151ba1561429b30710a8757dd9
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-02-28 15:54:36 +0900

    :sparkles: feature: mailSender 의존성 분리

---
+++ src/main/java/com/example/demo/user/service/CertificationService.java
@@ -0,0 +1,26 @@
+package com.example.demo.user.service;
+
+import com.example.demo.user.infastructure.UserEntity;
+import com.example.demo.user.service.port.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CertificationService {
+
+    private final MailSender mailSender;
+
+    public void send(String email, long userId, String certificationCode) {
+        String certificationUrl = generateCertificationUrl(userId, certificationCode);
+        String title = "Please your email address";
+        String content = "Please Clock the following link to certify your email address: " + certificationUrl;
+        mailSender.send(email, title, content);
+    }
+
+    private String generateCertificationUrl(long userId, String certificationCode) {
+        return "http://localhost:8080/api/users/" + userId + "/verify?certificationCode=" + certificationCode;
+    }
+}
