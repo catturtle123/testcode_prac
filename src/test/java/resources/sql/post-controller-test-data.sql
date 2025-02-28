a3e673745fc94f73ff7411f8fc2f3fe46c884cbb
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-02-28 11:44:09 +0900

    :sparkles: feature: postController test

---
+++ src/test/java/resources/sql/post-controller-test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
+VALUES (2, 'wow', 1234, 1234, 2);
+INSERT INTO `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
+VALUES (3, 'zoz', 1234, 1234, 3);
