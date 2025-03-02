79b35235d46feb14e869d98723b3da98b9d389a1
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2025-03-02 19:28:38 +0900

    :recycle: Refactor: 필요없는 어노테이션 삭제

--- src/test/java/com/example/demo/post/service/PostServiceTest.java
+++ src/test/java/com/example/demo/post/service/PostServiceTest.java
@@ -21,13 +21,6 @@ import org.springframework.test.context.jdbc.SqlGroup;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-@SpringBootTest
-@TestPropertySource("file:src/test/java/resources/application.properties")
-@SqlGroup({
-        @Sql(scripts = "file:src/test/java/resources/sql/user-repository-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(scripts = "file:src/test/java/resources/sql/post-repository-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-        @Sql(scripts = "file:src/test/java/resources/sql/delete_all.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-})
 class PostServiceTest {
 
     private PostService postService;
